f0ebfb11319c847ebd2a53773d03a256cb5dda3a
Thiago Cavalcante <thiagocav.ferreira@gmail.com>
2022-08-16 08:45:35 -0400

    new readme

--- 01_abordagens_envelope.R
+++ 01_abordagens_envelope.R
@@ -23,22 +23,22 @@ library(raster)
 
 #carregando os dados de ocorrência de Lagothrix flavicauda
 #(ver Figura 1 e Metadados na pasta)
-spdat <- read.csv("lagothrix_flavicauda.csv", stringsAsFactors = FALSE)
+spdat <- read.csv("data/l_flav.csv", stringsAsFactors = FALSE)
 head(spdat)
 
 #Carregando as variáveis ambientais para o Peru
 peru_stack <-
   stack(paste0("data/peru_predictors_stack.gri"))
 
-head(spdat)
-
 #Transformando o dataframe em um objeto espacial para vizualização
 sp <- SpatialPoints(cbind(spdat$lon, spdat$lat),
                     proj4string = CRS("+init=epsg:4326"))
 
+mapview(sp)
+
 #Plot das variáveis ambientais
 plot(peru_stack[[1:6]])
-#ver https://www.worldclim.org/data/bioclim.html para detalhes
+#ver detalhes: https://www.worldclim.org/data/bioclim.html
 
 #Conferindo tudo
 #selecionando o shapefile 1 do raster stack (Bio1)
@@ -48,14 +48,15 @@ plot(sp, add=TRUE, col="red", pch =1, cex = 1.5)
 #Estruturando os dados para usar a função "sre" do pacote biomod2
 #Essa função reproduz a análise do BIOCLIM original
 
-#Nós vamos usar as nossas variáveis para criar um raster onde todas as celulas
-#que tem occorrência da espécie possuam o valor 1 (presença) e todo o restante
-#seja considerado 0 (ausência). Esta será nossa variável resposta (response).
+#Nós vamos usar o recorte das nossas variáveis para criar um raster onde
+#todas as celulas que tem occorrência da espécie possuam o valor 1 (presença)
+#e todo o restante seja considerado 0 (ausência).
+#Este raster será nossa variável resposta (response).
 
 #detalhes:
 ??sre
 
-#Vamos também selecionar 3 variáveis climáticas que nós consideramos que podem
+#Vamos também selecionar 2 variáveis climáticas que nós consideramos que podem
 #afetar a distribuição da nossa espécie
 #Exemplo:
 #BIO1 = Annual Mean Temperature
@@ -82,8 +83,7 @@ myResp[cellFromXY(myResp,myRespXY)] <- 1
 plot(myResp)
 points(myRespXY, col = "red")
 
-#Utilizando a função species range envelope (SRE) do pacote biomod2
-#para reproduzir as análises do BIOCLIM
+#modelo utilizando todas as ocorrências (100% dos dados)
 sre.100 <-
   sre(
     Response = myResp,
@@ -92,7 +92,14 @@ sre.100 <-
     Quant = 0
   )
 
+#The quants argument determines the threshold at which the data will be taken
+#into account for calibration : the default of 0.05 induces that the 5% most
+#extreme values will be avoided for each variable on each side of its
+#distribution along the gradient. So it in fact takes 5% away at each end of
+#the variables distribution, giving a total of 10% of data not considered.
 
+
+#modelo utilizando 95% dos dados
 sre.095 <-
   sre(
     Response = myResp,
@@ -103,7 +110,7 @@ sre.095 <-
 #each variable not to be taken into account for determining the tolerance
 #boundaries for the considered species.
 
-
+#modelo utilizando 90% dos dados
 sre.090 <-
   sre(
     Response = myResp,
@@ -113,7 +120,6 @@ sre.090 <-
   )
 
 
-dev.off()
 ##' visualise results
 par(mfrow=c(2,2),mar=c(6, 5, 5, 3))
 plot(myResp, main = "Occurrence points")
@@ -122,10 +128,14 @@ plot(sre.100, main="BIOCLIM 100%")
 plot(sre.095, main="BIOCLIM 97.5%")
 plot(sre.090, main="BIOCLIM 95%")
 
+#vizulizando mais claramente
 mapview(sre.100)+
+  mapview(sre.095)+
+  mapview(sre.090)+
   mapview(sp)
 
-#Vamos repetir o processo adicionando uma variável
+#Vamos repetir o processo adicionando outras variáveis que podem afetar
+#a distribuição da espécie
 #BIO4 = Temperature Seasonality (standard deviation ×100)
 #BIO9 = Mean Temperature of Driest Quarter
 #BIO14 = Precipitation of Driest Month
@@ -146,12 +156,6 @@ myResp <-
 #Dando o valor de 1 (presença) para as localidades onde a espécie ocorre
 myResp[cellFromXY(myResp,myRespXY)] <- 1
 
-#add our points
-plot(myResp)
-points(myRespXY, col = "red")
-
-#Utilizando a função species range envelope (SRE) do pacote biomod2
-#para reproduzir as análises do BIOCLIM
 sre.100_v2 <-
   sre(
     Response = myResp,
@@ -160,11 +164,6 @@ sre.100_v2 <-
     Quant = 0
   )
 
-#The quants argument determines the threshold at which the data will be taken
-#into account for calibration : the default of 0.05 induces that the 5% most
-#extreme values will be avoided for each variable on each side of its
-#distribution along the gradient. So it in fact takes 5% away at each end of
-#the variables distribution, giving a total of 10% of data not considered.
 
 sre.095_v2 <-
   sre(
@@ -186,7 +185,6 @@ sre.090_v2 <-
   )
 
 
-dev.off()
 ##' visualise results
 par(mfrow=c(1,2))
 plot(sre.100, main="BIOCLIM 100% v1")
@@ -199,3 +197,4 @@ plot(sp, add=TRUE, col="red", pch =1, cex = 1.5)
 
 plot(sre.090, main="BIOCLIM 95% (X variáveis)")
 plot(sre.090_v2, main="BIOCLIM 95% (X variáveis)")
+plot(sp, add=TRUE, col="red", pch =1, cex = 1.5)
--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-
-# Disciplina_ENMs
+#Modelagem de Nicho Ecológico – Teoria e Prática
 
 <!-- badges: start -->
 <!-- badges: end -->
-
-O objetivo do projeto Disciplina_ENMs é fornecer scripts e material de suporte 
+  
+O objetivo deste repositório é fornecer scripts e material de suporte 
 para a disciplina Modelagem de nicho ecológico: Teoria e prática 2022.
 
 UNIVERSIDADE ESTADUAL DO SUDOESTE DA BAHIA – UESB
@@ -13,5 +12,14 @@ Programa de Pós-Graduação em Genética, Biodiversidade e Conservação – PP
 
 Luisa Maria Diele-Viegas & Thiago Cavalcante
 
+# Data
+This folder contains...
 
+Contents
+1. XXX
+- XXXX
+2. XXX
+- XXX
+3. XXX
+- XXX
 
